32497612ae85bbc8f8fe92ad120a4f5503ef1c11
Edwar Martinez <edwar_kenedy@hotmail.com>
2021-08-29 02:47:30 -0400

    Desinstalamos apollo-angular y corregimos un error en el header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { LayoutModule } from '@layout/layout.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
@@ -17,7 +16,6 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     LayoutModule,
     FontAwesomeModule,
-    GraphQLModule,
     HttpClientModule,
   ],
   providers: [],
